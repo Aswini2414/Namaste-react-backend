7050278cbc3907faf67b994e908c89e542bd4a8c
Aswini2414 <tejaswini2414154@gmail.com>
2024-07-05 23:33:51 +0530

    updated

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.post("/api/restaurants/update", async (req, res) => {
             "User-Agent":
               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
           },
-          body: JSON.Stringify(requestData),
+          body: JSON.stringify(requestData),
         }).then((response) => {
             if (!response.ok) {
               throw new Error("Network response was not ok");
