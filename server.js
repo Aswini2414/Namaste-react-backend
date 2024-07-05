4b67ad3574cbfb3cab490544f09d5cc04eef618e
Aswini2414 <tejaswini2414154@gmail.com>
2024-07-05 21:31:23 +0530

    updated

--- server.js
+++ server.js
@@ -14,34 +14,23 @@ app.get("/", (req, res) => {
     res.json({ "test": "This is an api for food delivery app" });
 });
 
-app.get("/api/restaurants",  (req, res) => {
+app.get("/api/restaurants", async(req, res) => {
     try {
       const { lat, lng } = req.query;
     console.log(lat, lng);
 
     const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
 
-    fetch(url, {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("An error occurred");
-      }); 
+        const response = await fetch(url, {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            },
+        });
+        res.json(response);
+    
     } catch (error) {
         res.status(400).json(error);
     }
