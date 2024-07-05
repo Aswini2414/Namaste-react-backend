e3858d17863093a7c62c7cc416c9b92685030dd0
Aswini2414 <tejaswini2414154@gmail.com>
2024-07-05 22:09:52 +0530

    updated

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get("/", (req, res) => {
 app.get("/api/restaurants", async (req, res) => {
     try {
         const { lat, lng } = req.query;
-        console.log(lat, lng);
 
         const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
 
@@ -40,7 +39,8 @@ app.get("/api/restaurants", async (req, res) => {
 
 app.post("/api/restaurants/update", async (req, res) => {
     try {
-        const { lat,lng,count } = req.body;
+        const { lat, lng, count } = req.body;
+        console.log(lat, lng, count);
 
         const url = "https://www.swiggy.com/dapi/restaurants/list/update";
 
@@ -68,18 +68,21 @@ app.post("/api/restaurants/update", async (req, res) => {
         };
 
         const response = await fetch(url, {
+          method: "POST",
           headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
             "User-Agent":
               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-            },
-            body:JSON.Stringify(requestData)
+          },
+          body: JSON.Stringify(requestData),
         });
 
+        console.log(response);
         const data = await response.json();
+        console.log(data);
         res.status(200).json(data);
-        
+
     } catch (error) {
       res.status(400).json(error);
     }
