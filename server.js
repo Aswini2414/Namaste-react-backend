342b13c602113048ed2e2c347ae91216ce67462d
Aswini2414 <tejaswini2414154@gmail.com>
2024-07-05 21:14:28 +0530

    updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/api/restaurants", async (req, res) => {
       const { lat, lng } = req.query;
     console.log(lat, lng);
 
-    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
 
     const response = await fetch(url, {
       headers: {
