9c0edd2bbbf36ed5e74d5033b2f5cd558e88dd49
Aswini2414 <tejaswini2414154@gmail.com>
2024-07-09 20:04:49 +0530

    updated

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ app.post("/api/restaurants/update", async (req, res) => {
         "Accept-Encoding": "gzip, deflate, br",
         "Content-Type": "application/json",
         "Cache-Control": "no-cache",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        "Content-Length": JSON.stringify(requestData).length,
       },
     });
     console.log(response);
